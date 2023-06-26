e63d2d69a9a81808b86a888b055e38910eb337d4
Quentin Rataud <quentin.rataud@epita.fr>
2023-06-26 13:41:48 +0900

    add csv and png file format

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import argparse
 import sbox
-
-red    = "\u001b[48;5;9m\u001b[38;5;15m"
-yellow = "\u001b[48;5;3m\u001b[38;5;0m"
-green  = "\u001b[48;5;10m\u001b[38;5;0m"
-green2 = "\u001b[48;5;2m\u001b[38;5;0m"
-end    = "\u001b[0m"
+import os
+from PIL import Image
 
 parser = argparse.ArgumentParser(
     description = "An open-source SBox analysis utility",
@@ -31,35 +27,133 @@ parser.add_argument(
     help = 'Compute the Difference Distribution Table of the SBoxes'
 )
 
+parser.add_argument(
+    '-format',
+    choices = ['ansi', 'csv', 'png'],
+    default = 'ansi',
+    help = 'Output format for the tables',
+)
+
+parser.add_argument(
+    '-output',
+    default = 'stdout',
+    help = 'Output directory path or "stdout" to print to standard output'
+)
+
 args = parser.parse_args()
 
-def print_table(lat):
-    upper = max(max(line) for line in lat[1:])
+def table_to_csv(table):
+    result = ''
+    for y, line in enumerate(table):
+        for x, elt in enumerate(line):
+            result += str(elt) + ','
+
+        result += '\n'
+    return result
+
+def table_to_ansi(table):
+    red    = "\u001b[48;5;9m\u001b[38;5;15m"
+    yellow = "\u001b[48;5;3m\u001b[38;5;0m"
+    green  = "\u001b[48;5;10m\u001b[38;5;0m"
+    green2 = "\u001b[48;5;2m\u001b[38;5;0m"
+    end    = "\u001b[0m"
+
+    upper = max(max(line) for line in table[1:])
     size = len(str(upper)) + 2
 
-    for y, line in enumerate(lat):
+    result = ''
+    for y, line in enumerate(table):
         for x, elt in enumerate(line):
             if elt == 0 or (x, y) == (0, 0):
-                print(green, end = '')
+                result += green
             elif abs(elt) == 2:
-                print(green2, end = '')
+                result += green2
             elif abs(elt) == upper:
-                print(red, end = '')
+                result += red
             else:
-                print(yellow, end = '')
-            print(str(elt).rjust(size), end = end)
-        print()
+                result += yellow
+            result += str(elt).rjust(size)
+            result += end
+        result += '\n'
+    return result
 
+def table_to_png(table):
+    red    = (255, 0, 0),
+    green  = (0, 255, 0),
+    green2 = (22, 222, 22),
+    yellow = (255, 255, 0),
+
+    upper = max(max(line) for line in table[1:])
+    size = len(str(upper)) + 2
+
+    width = len(table[0])
+    height = len(table)
+
+    image = Image.new("RGB", (width, height))
+
+    for y, line in enumerate(table):
+        for x, elt in enumerate(line):
+            if elt == 0 or (x, y) == (0, 0):
+                color = green
+            elif abs(elt) == 2:
+                color = green2
+            elif abs(elt) == upper:
+                color = red
+            else:
+                ratio = 1 - (abs(elt) - 4) / (upper - 4)
+                color = (255, int(255*ratio), 0)
+            image.putpixel((x, y), color)
+
+    return image
+
+def print_table(table, format='ansi', filename='stdout'):
+    if format == 'ansi':
+        output = table_to_ansi(table)
+    elif format == 'csv':
+        output = table_to_csv(table)
+    elif format == 'png':
+        output = table_to_png(table)
+
+    if filename == 'stdout':
+        if format == 'png':
+            print(output.tobytes.hex())
+        else:
+            print(output)
+    elif format == 'png':
+        output.save(filename, "png")
+    else:
+        with open(filename, 'w') as file:
+            file.write(output)
 
 for sbox_file in args.input_files:
     print(sbox_file, '\n')
     S = sbox.SBox.from_file(sbox_file)
     if args.lat:
-        print("Linear Approximation Table:")
-        print_table(S.linear_approximation_table())
+        print("Linear Approximation Table")
+        table = S.linear_approximation_table()
+        format = args.format
+        if args.output == 'stdout':
+            filename = 'stdout'
+        else:
+            filename = os.path.splitext(os.path.basename(sbox_file))[0]
+            filename = os.path.join(
+                args.output,
+                f"lat_{filename}.{format}"
+            )
+        print_table(table, format, filename)
         print()
 
     if args.ddt:
-        print("Difference Distribution Table:")
-        print_table(S.difference_distribution_table())
+        print("Difference Distribution Table")
+        table = S.difference_distribution_table()
+        format = args.format
+        if args.output == 'stdout':
+            filename = 'stdout'
+        else:
+            filename = os.path.splitext(os.path.basename(sbox_file))[0]
+            filename = os.path.join(
+                args.output,
+                f"ddt_{filename}.{format}"
+            )
+        print_table(table, format, filename)
         print()
